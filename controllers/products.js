5f7713051a398e81049fb8a9664da785562d20f6
syafiq <misteredwarv321@gmail.com>
2024-03-24 07:19:45 +0800

    me

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,20 @@
 //moongose.model
 const Products = require("../models/product");
 
-const getAllProducts = (req, res) => {
-    throw new Error("error happened");
-    res.status(200).json({ msg: `do this` });
+const getAllProductsStatic = async (req, res) => {
+    // throw new Error("tessting async errors");
+    const { name } = req.query;
+    if (name) {
+        const products = await Products.find({
+            name: name,
+        });
+        return res.status(200).json({ nbHits: products.length, products });
+    }
+    const products = await Products.find({});
+    return res.status(200).json({ nbHits: products.length, products });
 };
-const getAllProductsStatic = (req, res) => {
+
+const getAllProducts = async (req, res) => {
     res.status(200).json({ msg: `do that` });
 };
 

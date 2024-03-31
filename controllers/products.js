1a8c6414b7e00e60cbb29cf57a3a0aac90673c40
syafiq <misteredwarv321@gmail.com>
2024-04-01 03:32:36 +0800

    lalalalalalala

--- controllers/products.js
+++ controllers/products.js
@@ -3,19 +3,16 @@ const Products = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
     // throw new Error("tessting async errors");
-    const { name } = req.query;
-    if (name) {
-        const products = await Products.find({
-            name: name,
-        });
-        return res.status(200).json({ nbHits: products.length, products });
-    }
-    const products = await Products.find({});
-    return res.status(200).json({ nbHits: products.length, products });
+
+    const products = await Products.find({
+        name: "wooden table",
+    });
+    res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: `do that` });
+    const products = await Products.find(req.query);
+    res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = {
